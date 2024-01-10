b67748a66ff2c1f30f4cc23d5a06a4fa1d334fd2
Vishist Singh Solanki <vishistsingh16gh@gmail.com>
2024-01-10 15:48:11 +0530

    solved anagram from 01-js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,29 @@
 */
 
 function isAnagram(str1, str2) {
-
+  let a = str1.length;
+  let b = str2.length;
+  let c = str2.split("");
+  for(let i=0; i<c.length; i++){
+    c[i] = c[i].toLowerCase();
+  }
+  if(a==b){
+    let count = 0;
+    for(let i=0; i<a; i++){
+      if(c.includes(str1[i].toLowerCase())){
+        count++;
+      }
+    }
+    if(count==b){
+        return true;
+      }
+      else{
+        return false;
+      }
+  }
+  else{
+    return false;
+  }
 }
 
 module.exports = isAnagram;
